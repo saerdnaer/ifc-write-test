47810527dedf90e50835f6870e020c6ea2f4e1df
Andreas Hubel <andi@saerdnaer.de>
2013-03-21 10:46:32 +0100

    added cdp building class

---
+++ src/CDP_Building.cpp
@@ -0,0 +1,16 @@
+#include "CDP_Building.hpp"
+
+
+CDP_Building::CDP_Building(void)
+{
+}
+
+
+CDP_Building::~CDP_Building(void)
+{
+}
+
+int CDP_Building::id(void)
+{
+	return simple->id;
+}
---
+++ src/CDP_Building.hpp
@@ -0,0 +1,33 @@
+#pragma once
+//#include <qobject.h>
+#include <boost\shared_ptr.hpp>
+//#include "sBuilding.h"
+//#include "Building.hpp"
+#include "SimpleMesh.h"
+
+class CDP_Building // : public QObject
+{
+	Q_OBJECT
+
+public:
+	typedef boost::shared_ptr<CDP_Building> ptr;
+
+	//boost::shared_ptr<sBuilding> simple;
+	//boost::shared_ptr<PrsBuilding> presentation;
+
+	CDP_Building(void);
+	~CDP_Building(void);
+
+	int id;
+	int getId();
+
+	float height;
+	float getHeight();
+
+	SimpleMesh* mesh;
+	SimpleMesh* getMesh();
+
+	Point3d getPosition();
+
+};
+
